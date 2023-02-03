f98b509f577b36b13b841748a779205c5d27c331
souza99 <jpfsouza99@gmail.com>
2023-02-03 00:04:27 -0300

    Ajustando o campo de pesquisa na tela

--- lib/Pages/Formularios/Gado/ListagemGado.dart
+++ lib/Pages/Formularios/Gado/ListagemGado.dart
@@ -23,7 +23,39 @@ class _GadoListState extends State<GadoList> {
         decoration: BoxDecoration(
           color: Colors.grey[850],
         ),
-        child: buildGadoList(),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                      ),
+                      child: TextField(
+//                        maxLength: 300,
+                        decoration: InputDecoration(
+                          hintText: "Procurar...",
+                          border: InputBorder.none,
+                          suffixIcon: Icon(Icons.search),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 24.0, vertical: 14),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: buildGadoList(),
+            ),
+          ],
+        ),
       ),
     ));
   }
