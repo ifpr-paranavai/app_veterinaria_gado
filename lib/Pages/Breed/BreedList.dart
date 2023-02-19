f8ef5bbd6b91a70e221774607d319fbc51c4b4ca
souza99 <jpfsouza99@gmail.com>
2023-02-19 01:36:38 -0300

    adding a header to my list

--- lib/Pages/Breed/BreedList.dart
+++ lib/Pages/Breed/BreedList.dart
@@ -115,71 +115,107 @@ class _BreedListState extends State<BreedList> {
   }
 
   buildUserList() {
-    return ListView(
-      children: _breeds
-          .asMap()
-          .map(
-            (index, breed) => MapEntry(
-              index,
-              Container(
-                color: index % 2 == 0 ? Colors.white : Colors.grey[200],
-                child: ListTile(
-                  //leading: Icon(Icons.add_a_photo),
-                  title: Row(
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(
-                              breed.name,
-                              style: TextStyle(
-                                  color: Color.fromARGB(255, 0, 0, 0)),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Row(
-                          children: [
-                            IconButton(
-                              onPressed: () => {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => BreedRegistration(
-                                            breed: breed,
-                                          )),
-                                ).then((value) => {
-                                      if (value == null) {_fetchDataBreed()}
-                                    })
-                              },
-                              icon: Icon(Icons.edit),
-                              color: Color.fromARGB(255, 20, 122, 16),
-                            ),
-                            IconButton(
-                              onPressed: () => {
-                                breedDatabase.delete(breed.id, 'breed'),
-                                setState(() {
-                                  _fetchDataBreed();
-                                })
-                              },
-                              icon: Icon(Icons.delete),
-                              color: Color.fromARGB(255, 189, 18, 18),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: SizedBox(width: 55),
+            title: Text(
+              'Nome',
+              style: TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.bold,
               ),
             ),
-          )
-          .values
-          .toList(),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'excluir',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 16),
+                Text(
+                  'editar',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            contentPadding: EdgeInsets.symmetric(vertical: 1, horizontal: 40),
+          ),
+          ..._breeds
+              .asMap()
+              .map(
+                (index, breed) => MapEntry(
+                  index,
+                  Container(
+                    color: index % 2 == 0 ? Colors.white : Colors.grey[200],
+                    child: ListTile(
+                      //leading: Icon(Icons.add_a_photo),
+                      title: Row(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  breed.name,
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 0, 0, 0)),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () => {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              BreedRegistration(
+                                                breed: breed,
+                                              )),
+                                    ).then((value) => {
+                                          if (value == null) {_fetchDataBreed()}
+                                        })
+                                  },
+                                  icon: Icon(Icons.edit),
+                                  color: Color.fromARGB(255, 20, 122, 16),
+                                ),
+                                IconButton(
+                                  onPressed: () => {
+                                    breedDatabase.delete(breed.id, 'breed'),
+                                    setState(() {
+                                      _fetchDataBreed();
+                                    })
+                                  },
+                                  icon: Icon(Icons.delete),
+                                  color: Color.fromARGB(255, 189, 18, 18),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+              .values
+              .toList(),
+        ],
+      ),
     );
   }
 }
