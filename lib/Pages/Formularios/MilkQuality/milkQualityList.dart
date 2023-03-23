4c5e027d4035374a8dfeba0c19dbae8cf27fdc28
jpfsouza99 <jpfsouza99@gmail.com>
2023-03-23 20:40:08 -0300

    Iniciando tela de listagem do leite

---
+++ lib/Pages/Formularios/MilkQuality/milkQualityList.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MilkQuality extends StatefulWidget {
+  const MilkQuality({super.key});
+
+  @override
+  State<MilkQuality> createState() => _MilkQualityState();
+}
+
+class _MilkQualityState extends State<MilkQuality> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
